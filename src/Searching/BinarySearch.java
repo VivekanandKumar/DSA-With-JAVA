9648911e7184c0e6d0685900dcf2468be1156da3
Vivekanand Kumar <mailofvivekanand@gmail.com>
2021-12-16 08:44:08 +0530

    updated binarySearch class

--- src/Searching/BinarySearch.java
+++ src/Searching/BinarySearch.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 
 public class BinarySearch {
 
-    public static int binarySearch(int[] arr,int target){
+    static int binarySearch(int[] arr,int target){
         int start = 0;
         int end = arr.length-1;
-
-        for (int i=0;i<arr.length;i++){
-            int mid = (start + end) / 2;
-            if(arr[mid] == target)
+        while (start <= end){
+            int mid = start + (end - start)/2;//if we use (start+end)/2 it might possible that the
+            // mid value exceeds the range of integer thats why we
+            //use an efficient way of find mid element.
+            //start + end-start / 2; ==> 2start + end - start / 2 => [start + end / 2];
+            //hence the formula remain same as previews but it never exceeds the range of integer.
+            if (target == arr[mid])
                 return mid;
             else if (target > arr[mid])
-                start = mid + 1;
+                start = mid+1;
             else if (target < arr[mid])
                 end = mid-1;
         }
@@ -22,9 +25,9 @@ public class BinarySearch {
 
 
     public static void main(String[] args) {
-        int[] arr = {-1,0,4,8,10,17};
-        int target = 89;
-        Arrays.sort(arr);
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 22;
+//        Arrays.sort(arr);
         System.out.println(binarySearch(arr,target));
     }
 }
