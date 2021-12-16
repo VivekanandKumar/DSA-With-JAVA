c8636cc3e83a31111311a016a53411ef2ad7e975
Vivekanand Kumar <mailofvivekanand@gmail.com>
2021-12-16 08:47:29 +0530

    added comments to binarySearch class

--- src/Searching/BinarySearch.java
+++ src/Searching/BinarySearch.java
@@ -8,11 +8,13 @@ public class BinarySearch {
         int start = 0;
         int end = arr.length-1;
         while (start <= end){
-            int mid = start + (end - start)/2;//if we use (start+end)/2 it might possible that the
+            //find mid element.
+            int mid = start + (end - start)/2;  //if we use (start+end)/2 it might possible that the
             // mid value exceeds the range of integer thats why we
             //use an efficient way of find mid element.
             //start + end-start / 2; ==> 2start + end - start / 2 => [start + end / 2];
             //hence the formula remain same as previews but it never exceeds the range of integer.
+
             if (target == arr[mid])
                 return mid;
             else if (target > arr[mid])
@@ -20,14 +22,13 @@ public class BinarySearch {
             else if (target < arr[mid])
                 end = mid-1;
         }
-        return -1;
+        return -1; // if the target element not exist in given array.
     }
 
 
     public static void main(String[] args) {
-        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89}; // the array must be sorted .
         int target = 22;
-//        Arrays.sort(arr);
         System.out.println(binarySearch(arr,target));
     }
 }
