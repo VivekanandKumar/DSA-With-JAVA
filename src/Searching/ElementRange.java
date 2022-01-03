86952e68b147b80590942e991055fa85eb5899a4
Vivekanand Kumar <mailofvivekanand@gmail.com>
2022-01-03 20:21:41 +0530

    initial commit

---
+++ src/Searching/ElementRange.java
@@ -0,0 +1,50 @@
+package Searching;
+
+import java.util.Arrays;
+
+public class ElementRange {
+
+    static int[] range(int[] arr,int target){
+        int[] ans = {-1,-1};
+        ans[0] = search(arr,target,true);
+        if(ans[0] != -1){
+            ans[1] = search(arr,target,false);
+        }
+        return ans;
+    }
+
+
+    static int search(int[] arr,int target,boolean isFirst){
+        int start = 0;
+        int end = arr.length-1;
+        int ans = -1;
+        while(start <= end){
+            int mid = (start + end) /2;
+
+            if(target < arr[mid]){
+                end = mid-1;
+            }
+            else if(target > arr[mid]){
+                start = mid+1;
+            }
+            else{
+                ans = mid;
+
+                if(isFirst){
+                    end = mid - 1;
+                }else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,5,5,5,7,10};
+        int target = 5;
+
+        System.out.println(Arrays.toString(range(arr,target)));
+
+    }
+}
